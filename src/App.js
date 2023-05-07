f91e8bb502f51c5ffe3dbfc5c368fdcf30ef0757
aditya-K2 <adityakurdunkar2@gmail.com>
2023-05-07 18:43:14 +0530

    Send Notification Alerts on Events

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import tmlogo from './assets/logo.png';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import CSVUploader from './components/CSVUploader';
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <img src={tmlogo} alt='Theticketmaster'></img>
-      <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<LoginSignup />} />
-          <Route path= "home" index element={<Home />} />
-      </Routes>
-      </BrowserRouter>
+    return (
+        <div className={styles.App}>
+            <img src={tmlogo} alt='Theticketmaster'></img>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginSignup />} />
+                    <Route path="home" index element={<Home />} />
+                    <Route path="csv" index element={<CSVUploader />} />
+                </Routes>
+            </BrowserRouter>
 
-        {/* <LoginSignup /> */}
-      <img className={styles.bottomImg} src={train} alt='train'></img>
-    </div>
-  );
+            {/* <LoginSignup /> */}
+            <img className={styles.bottomImg} src={train} alt='train'></img>
+        </div>
+    );
 }
 
 export default App;
